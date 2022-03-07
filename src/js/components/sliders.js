e0b725306655a945e246e15d00ffa39a781ab523
The73756 <the73756the@gmail.com>
2022-03-07 15:03:29 +0900

    minor update

--- src/js/components/sliders.js
+++ src/js/components/sliders.js
@@ -1,60 +1,61 @@
 const bodyStyles = window.getComputedStyle(document.body);
 const gap = parseInt(bodyStyles.getPropertyValue('--grid-gap'));
 const portSlider = document.querySelector('.portfolio-section__items')
-
 import Swiper, { Navigation, Pagination } from 'swiper';
 Swiper.use([Navigation, Pagination]);
 
-const portfolioSlider = new Swiper(portSlider, {
-  modules: [Navigation, Pagination],
-  slidesPerView: 3,
-  spaceBetween: gap,
-  on: {
-    init: function () {
-      const activeSlide = portSlider.querySelector('.swiper-slide-active');
-      const nextActiveSlide = activeSlide.nextElementSibling;
-      const nextNextActiveSlide = nextActiveSlide.nextElementSibling;
 
+if (portSlider) {
+  const portfolioSlider = new Swiper(portSlider, {
+    modules: [Navigation, Pagination],
+    slidesPerView: 3,
+    spaceBetween: gap,
+    on: {
+      init: function () {
+        const activeSlide = portSlider.querySelector('.swiper-slide-active');
+        const nextActiveSlide = activeSlide.nextElementSibling;
+        const nextNextActiveSlide = nextActiveSlide.nextElementSibling;
+
+        activeSlide.classList.add('slider-visible');
+        nextActiveSlide.classList.add('slider-visible');
+        nextNextActiveSlide.classList.add('slider-visible');
+      },
+    },
+    navigation: {
+      nextEl: '.portfolio-section__next',
+      prevEl: '.portfolio-section__prev',
+    },
+  });
+
+  document.querySelector('.portfolio-section__prev').addEventListener('click', () => {
+    const activeSlide = portSlider.querySelector('.swiper-slide-next');
+
+    document.querySelectorAll('.portfolio-section__items .swiper-slide').forEach(el => {
+      el.classList.remove('slider-visible');
+    });
+
+    if (activeSlide.previousElementSibling) {
+      const nextActiveSlide = activeSlide.previousElementSibling;
       activeSlide.classList.add('slider-visible');
       nextActiveSlide.classList.add('slider-visible');
-      nextNextActiveSlide.classList.add('slider-visible');
-    },
-  },
-  navigation: {
-    nextEl: '.portfolio-section__next',
-    prevEl: '.portfolio-section__prev',
-  },
-});
-
-document.querySelector('.portfolio-section__prev').addEventListener('click', () => {
-  const activeSlide = portSlider.querySelector('.swiper-slide-next');
-
-  document.querySelectorAll('.portfolio-section__items .swiper-slide').forEach(el => {
-    el.classList.remove('slider-visible');
+      activeSlide.nextElementSibling.classList.add('slider-visible');
+    }
   });
 
-  if (activeSlide.previousElementSibling) {
-    const nextActiveSlide = activeSlide.previousElementSibling;
+  document.querySelector('.portfolio-section__next').addEventListener('click', () => {
+    const activeSlide = portSlider.querySelector('.swiper-slide-active');
+    const nextActiveSlide = activeSlide.nextElementSibling;
+    const nextNextActiveSlide = nextActiveSlide.nextElementSibling;
+
+    document.querySelectorAll('.portfolio-section__items .swiper-slide').forEach(el => {
+      el.classList.remove('slider-visible');
+    });
+
     activeSlide.classList.add('slider-visible');
     nextActiveSlide.classList.add('slider-visible');
-    activeSlide.nextElementSibling.classList.add('slider-visible');
-  }
-});
-
-document.querySelector('.portfolio-section__next').addEventListener('click', () => {
-  const activeSlide = portSlider.querySelector('.swiper-slide-active');
-  const nextActiveSlide = activeSlide.nextElementSibling;
-  const nextNextActiveSlide = nextActiveSlide.nextElementSibling;
-
-  document.querySelectorAll('.portfolio-section__items .swiper-slide').forEach(el => {
-    el.classList.remove('slider-visible');
+    nextNextActiveSlide.classList.add('slider-visible');
   });
-
-  activeSlide.classList.add('slider-visible');
-  nextActiveSlide.classList.add('slider-visible');
-  nextNextActiveSlide.classList.add('slider-visible');
-});
-
+}
 
 const parttestimonialsSlider = new Swiper('.testimonials__items', {
   modules: [Navigation, Pagination],
