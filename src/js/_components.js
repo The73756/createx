2d289cdc6c90f6d2b687f65cb697dd6bd212de8f
The73756 <the73756the@gmail.com>
2022-03-19 21:17:14 +0900

    add contacts page

--- src/js/_components.js
+++ src/js/_components.js
@@ -1,5 +1,5 @@
-console.log('components');
 import './components/sliders.js';
 import './components/progress.js';
 import './components/accordions.js';
 import './components/portfolio-tabs.js';
+import './components/selects.js';
--- src/js/_vendor.js
+++ src/js/_vendor.js
@@ -1,1 +1,2 @@
 import './vendor/focus-visible.js';
+import './vendor/choices.min.js';
